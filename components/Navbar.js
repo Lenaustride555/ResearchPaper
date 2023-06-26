3f8eb0b568aaf41f1c3d42c5aced95c56468081f
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2023-06-26 19:30:04 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Navbar = () => {
   return (
     <>
@@ -12,18 +12,23 @@ const Navbar = () => {
       <span className="ml-3 text-xl">Research Node</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-white">First Link</a>
-      <a className="mr-5 hover:text-white">Second Link</a>
-      <a className="mr-5 hover:text-white">Third Link</a>
-      <a className="mr-5 hover:text-white">Fourth Link</a>
+    <Link className="mr-5 hover:text-white" href="/about">
+    About
+    </Link>
+    <Link className="mr-5 hover:text-white" href="/register">
+    Register
+    </Link>
+    <Link className="mr-5 hover:text-white" href="/login">
+    Login
+    </Link>
     </nav>
-    <button className="inline-flex items-center bg-teal-800 border-0 py-1 px-3 focus:outline-none hover:bg-stone-950 rounded text-base mt-4 md:mt-0">Button
+    <button className="inline-flex items-center bg-teal-800 border-0 py-1 px-3 focus:outline-none hover:bg-stone-950 rounded text-base mt-4 md:mt-0">Logout
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
     </button>
   </div>
-  
+
 </header>
     </>
   )
