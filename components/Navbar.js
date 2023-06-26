b610c515be2a731506ffc1af94355bce61fcc5de
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2023-06-26 21:24:02 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <>
       <header className="text-gray-400 bg-transparent body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <a href="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -19,7 +19,7 @@ const Navbar = () => {
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span className="ml-3 text-xl">
-              <Link href="/">Research Node</Link>
+              Research Node
             </span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
