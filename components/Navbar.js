cc7588393c011a1fa8e1ece6ff95f9bec6f9ea5c
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2023-06-26 19:38:41 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
     Login
     </Link>
     </nav>
-    <button className="inline-flex items-center bg-teal-800 border-0 py-1 px-3 focus:outline-none hover:bg-stone-950 rounded text-base mt-4 md:mt-0">Logout
+    <button className="inline-flex items-center bg-stone-950 border-0 py-1 px-3 focus:outline-none hover:bg-teal-800 rounded text-base mt-4 md:mt-0">Logout
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
